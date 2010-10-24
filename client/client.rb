fc4a3f5a27a8977353a9f0528ca94f658d9de088
Kimmynoodles <Ryan@sandhaus.com>
2010-10-24 05:34:35 -0400

    Fixed the map to fit a smaller resolution`

--- client/client.rb
+++ client/client.rb
@@ -5,7 +5,7 @@ end
 require "httparty"
 require 'json'
 
-Shoes.app :height => 730, :width => 1440 do
+Shoes.app :height => 730, :width => 1296 do
 
   require '../shared/player'
 
@@ -15,22 +15,22 @@ Shoes.app :height => 730, :width => 1440 do
 
   board = []
 
-  800.times { board << "static/sand_1.gif" } 
+  720.times { board << "static/sand_1.gif" } 
 
-  (0..39).each {|i| board[i] = "static/wall_t_1.gif"}
-  (0..19).each {|i| board[i * 40 - 1] = "static/wall_r_1.gif"}
-  (0..36).each {|i| board[40 + i * 20] = "static/wall_l_1.gif"}
-  (0..19).each {|i| board[i * 40] = "static/wall_l_1.gif"}
-  (0..39).each {|i| board[i + 760] = "static/wall_t_1.gif"}
-  (0..1).each  {|i| board[320 + i * 40] = "static/sand_1.gif"}
-  (0..1).each  {|i| board[340 + i * 40] = "static/sand_1.gif"}
-  (0..6).each  {|i| board[272 + i] = "static/wall_t_1.gif"}
-  (0..6).each  {|i| board[552 + i] = "static/wall_t_1.gif"}
-  (0..5).each  {|i| board[312 + i * 40] = "static/wall_l_1.gif"}
+  (0..35).each {|i| board[i] = "static/wall_t_1.gif"}
+  (0..20).each {|i| board[i * 36 - 1 ] = "static/wall_r_1.gif"}
+  (0..19).each {|i| board[i * 36] = "static/wall_l_1.gif"}
+  (2..37).each {|i| board[i * 18] = "static/wall_l_1.gif"}
+  (0..34).each {|i| board[i + 684] = "static/wall_t_1.gif"}
+  (0..1).each  {|i| board[324 + i * 36] = "static/sand_1.gif"}
+  (0..1).each  {|i| board[342 + i * 36] = "static/sand_1.gif"}
+  (0..6).each  {|i| board[280 + i] = "static/wall_t_1.gif"}
+  (0..6).each  {|i| board[460 + i] = "static/wall_t_1.gif"}
+  (0..3).each  {|i| board[316 + i * 36] = "static/wall_l_1.gif"}
 
-  board[436] = "static/unicorn.gif"
+  board[357] = "static/unicorn.gif"
 
-  board[432] = "static/sandweird.gif"
+  board[352] = "static/sandweird.gif"
 
   #board[360] = "static/Warrior.gif"
 
@@ -48,7 +48,7 @@ Shoes.app :height => 730, :width => 1440 do
     #player.y_pos -= 36
     #player_image.top -= 36
     @player_top -= 36
-   @player_image.style :top => @player_top
+    @player_image.style :top => @player_top
   end
 
   def move_player_down

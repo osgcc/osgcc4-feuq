0ffd040990baea4fa316ccce37d864628be7f469
Kimmynoodles <Ryan@sandhaus.com>
2010-10-24 17:49:21 -0400

    Testing unicorn almost done

--- client/battle.rb
+++ client/battle.rb
@@ -1,38 +1,64 @@
 class Battle
   def initialize slot
     slot.replace do
-      
-      cheeky_drat
-      
       @unicorn = Unicorn.new
       @combatant = [@player]  
       @screen = []
       @screen << "static/combatbox.gif"
       @screen << "static/desert.gif"
       #432.times { @screen << "static/sand_1.gif" }
-      
+      refChance = 0
       @combatant.each do |i|
-
+        refChance = rand(99)+1
+       
         button "Attack", :top => 50, :left => 70 do
-          attack_sound
-          damage = i.attack
-          alert "" + i.class.to_s + " deals " + damage.to_s + " damage."
-          @unicorn.hp -= damage
-          if @unicorn.hp <= 0
+	  if refChance >= 1 && refChance <= 3
+	    damage = i.attack
+            alert "The unicorn reflected your attack dealing " + damage.to_s + " damage!"
+            i.hp -= damage
+	  else
+	    damage = i.attack
+	    alert i.class.to_s + " deals " + damage.to_s + " damage."
+	    @unicorn.hp -= damage
+alert(@unicorn.hp.to_s)
+            
+	  end
+
+        damage = @unicorn.attack
+        alert "The unicorn deals " + damage.to_s + " to you."
+        @player.hp -= damage
+
+        if @unicorn.hp <= 0 
             Win.new self, true
-          end
+	end
+	if @player.hp <= 0
+	  Win.new self, false
+	end
         end
         
         button "Special", :top => 150, :left => 70 do
           special_sound
           damage = i.specAtk
-          @unicorn.hp -= damage
-          if @unicorn.hp <= 0
+
+          @unicorn.hp -= damage  
+
+		
+
+       if @unicorn.hp <= 0 
             Win.new self, true
-          end
-        end
+	end
+	if @player.hp <= 0
+	  Win.new self, false
+	end
+        end 
+
         
+       
+
+
       end
+
+
       @screen.each do |b| 
         image b
       end
@@ -42,9 +68,9 @@ class Battle
       @health_image = image "static/health100.gif", :top=> 550, :left =>905
       @charlie_image = image "static/charlie.gif", :top=> 575, :left =>901
       #@desert_image = image "static/desert.gif", :top=> 575, :left =>901
-
+     end	
     end
-  end
+ 
 
 
 
--- shared/unicorn.rb
+++ shared/unicorn.rb
@@ -1,10 +1,16 @@
 class Unicorn < Player
 
   def initialize(opts={})
-   # opts[:mp] ||= 999999
-    opts[:hp] ||= 20
-   # opts[:const] ||= -5
+    opts[:mp] ||= 100
+    opts[:hp] ||= 100
+    opts[:const] ||= 2
+    opts[:int]  ||= 10
+    opts[:str]  ||= 11
     super(opts)
   end
+
+def attack
+ rand(5) + str
+end
 
 end
